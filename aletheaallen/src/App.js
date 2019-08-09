c615962eecc3ced97ff62118a8c4b35958ab0e82
Lee Allen <leeallen737@gmail.com>
2019-08-09 19:56:31 +0100

    toolbar and hamburger menu functionality added

--- aletheaallen/src/App.js
+++ aletheaallen/src/App.js
@@ -21,18 +21,16 @@ backdropClickHandler = () => {
 
 
   render(){
-    let sideDrawer;
     let backdrop;
 
     if (this.state.sideDrawerOpen)  {
-      sideDrawer = <SideDrawer />;
       backdrop = <Backdrop click={this.backdropClickHandler}/>
     }
 
      return(
        <div style={{height: '100%'}}>
        <NavBar drawerClickHandler={this.drawerToggleClickHandler} />
-       {sideDrawer}
+       <SideDrawer show={this.state.sideDrawerOpen}/>
        {backdrop }
        <main style={{marginTop: '64px'}}><p>This is the page content</p></main>
        <p>main content</p>
