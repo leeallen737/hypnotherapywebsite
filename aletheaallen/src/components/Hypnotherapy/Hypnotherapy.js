69eed216a4ddae39ed5a2317b7fc40b84f5d11c4
lee737 <leeallen@Lees-iMac.local>
2019-08-13 18:47:49 +0100

    added extra pages in prep for routing

---
+++ aletheaallen/src/components/Hypnotherapy/Hypnotherapy.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Hypnotherapy.css";
+
+const Hypnotherapy = () => {
+  return (
+    <section>
+        <div className="outside_frame">
+          My Hypnotherapy page
+        </div>
+    </section>
+  );
+};
+
+export default Hypnotherapy;
