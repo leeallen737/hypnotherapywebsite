b8bedf9c09a448473b8deb35c2c8b607a4909d97
lee737 <leeallen737@gmail.com>
2019-08-19 15:15:23 +0100

    added overlays to home boxes and tidied up title

--- aletheaallen/src/components/Contact/ContactForm.js
+++ aletheaallen/src/components/Contact/ContactForm.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import { withFormik } from 'formik';
 
 const ContactForm = () =>   {
     return(
         
         <div>
-            <input type="email" name="email" placeholder="Email"/>
+            Contact Page
         </div>
     )
 }
-
-const FormikApp = withFormik({
-
-})(ContactForm)
 
 export default ContactForm 
