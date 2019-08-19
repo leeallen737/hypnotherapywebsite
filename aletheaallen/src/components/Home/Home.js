1bbc1cf0995fcd397d74c5fb20762cdd2e4c64ea
lee737 <leeallen737@gmail.com>
2019-08-19 13:30:54 +0100

    updated home.js

--- aletheaallen/src/components/Home/Home.js
+++ aletheaallen/src/components/Home/Home.js
@@ -3,13 +3,15 @@ import "./Home.css";
 
 const Home = () => {
   return (
-    <section>
+    <main className="main-area">
+      <div className="outer_frame_heading">
     <div className="my-heading">
       <div className="my-heading-width"><h1>HOW CAN HYPNOTHERAPY HELP YOU?</h1></div>
     </div>
+    </div>
         <div className="outside_frame">
           <div className="top">
-            <div className="top1">top 1</div>
+            <div className="top1"></div>
             <div className="top2">top 2</div>
           </div>
           <div className="bottom">
@@ -18,7 +20,7 @@ const Home = () => {
             <div className="bottom_right">bottom right</div>
           </div>
         </div>
-    </section>
+    </main>
   );
 };
 
