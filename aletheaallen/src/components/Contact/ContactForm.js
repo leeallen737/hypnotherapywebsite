8c1300f317c596f9e429a8d36dad8757c54f980e
Lee Allen <leeallen737@gmail.com>
2019-09-03 20:58:39 +0100

    contact form, stage 1

--- aletheaallen/src/components/Contact/ContactForm.js
+++ aletheaallen/src/components/Contact/ContactForm.js
@@ -1,16 +1,32 @@
 import React from 'react';
+import './ContactForm.css';
 
 const ContactForm = () =>   {
     return(
-        <section>
+        <section className="outside_contact_form">
     <div className="my-heading">
       <div className="my-heading-width">CONTACT FORM</div>
     </div>
-        <div className="outside_frame">
-          My Contact Form here
-        </div>
+    <form name="contact" method="POST" data-netlify-recaptcha="true" data-netlify="true">
+      <div className="inside_contact_form">
+    <p>
+    <input type="text" name="name" placeholder="Name"/>
+  </p>
+  <p>
+    <input type="email" name="email" placeholder="Email"/>
+  </p>
+  
+  <p>
+    <textarea className="message" name="message"></textarea>
+  </p>
+  <div data-netlify-recaptcha="true"></div>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+  </div>
+</form>
     </section>
     )
 }
 
-export default ContactForm 
+export default ContactForm
